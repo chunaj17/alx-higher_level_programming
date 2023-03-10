a6f0daabe1b286603bbcc4ed0688083b6343195f
chunaj17 <chunaj300@gmail.com>
2023-03-10 06:56:57 +0300

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 
-arg_length = len(argv) - 1
-if (arg_length == 0):
-    print("{:d} arguments.".format(arg_length))
-elif (arg_length == 1):
-    print("{:d} argument:".format(arg_length))
+if (len(argv) - 1 == 0):
+    print("{:d} arguments.".format(len(argv) - 1))
+elif (len(argv) - 1 == 1):
+    print("{:d} argument:".format(len(argv) - 1))
 else:
-    print("{:d} arguments:".format(arg_length))
+    print("{:d} arguments:".format(len(argv) - 1))
 
-for i in range(1, arg_length + 1):
+for i in range(1, len(argv)):
     print("{:d}: {:s}".format(i, argv[i]))
